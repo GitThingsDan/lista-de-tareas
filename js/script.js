a834f74cfee93f8ce187b1b6e50c184bbb4f0af0
GitThingsDan <dvillarroela@proton.me>
2023-11-04 16:20:44 -0300

    Cambiar a VS Codium; mover archivos de .gitignore fuera del proyecto para que .gitignore vuelva a ignorarlos como corresponde

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let platoPrincipal = "Pizza Greek Feta";
+let platoSegundo = "Pizza Pollo Caprese";
+
+const nombreRestaurant = "Hard Rock Café";
+
+platoPrincipal = "Pizza Hawaiana"
+// nombreRestaurant = "Burger King"
+
+let porcionesDePizza = 2;
+porcionesDePizza = 3;
+
+console.warn("Esto es una advertencia...¡ALARMAAA!");
