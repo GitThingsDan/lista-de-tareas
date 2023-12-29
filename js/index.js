4ca4614cf9c4438cfec5113d093cb6cc9f76dcba
GitThingsDan <dvillarroela@proton.me>
2023-12-29 00:33:31 -0300

    Rehacer index.js, ordenando y comentando de mejor manera (con, simpáticamente, una lista de tareas para no perder mi norte como en mi intento previo).

--- js/index.js
+++ js/index.js
@@ -1,29 +1,5 @@
-/* function timeToMS(time, timeUnit) {
-	const minutesToMS = minutes => minutes * 60 * 1000
-	const hoursToMS = hours => hours * minutesToMS(60)
-	const daysToMS = days => days * hoursToMS(24)
-	const timeUnits = [
-		{ name: "m", toMSFunc: minutesToMS, },
-		{ name: "h", toMSFunc: hoursToMS, },
-		{ name: "d", toMSFunc: daysToMS, }
-	]
-	const unit = timeUnits.find(unit => unit.name === timeUnit)
-	return unit.toMSFunc(time)
-} */
-/* function msToTime(ms, timeUnit) {
-	const msToMinutes = ms => ms / (60 * 1000)
-	const msToHours = ms => msToMinutes(ms) / 60
-	const msToDays = ms => msToHours(ms) / 24
-	const timeUnits = [
-		{ name: "m", toTimeUnitFunc: msToMinutes, },
-		{ name: "h", toTimeUnitFunc: msToHours, },
-		{ name: "d", toTimeUnitFunc: msToDays, }
-	]
-	const unit = timeUnits.find(unit => unit.name === timeUnit)
-	return unit.toTimeUnitFunc(ms)
-} */
-
-/* --------------------------------- */
+// INSPIRADO EN https://www.w3schools.com/howto/howto_js_todolist.asp
+/* ------------CLASES:------------ */
 
 class Tiempo {
 	constructor() {
@@ -33,19 +9,21 @@ class Tiempo {
 	}
 }
 class Tarea {
-	/* 	static counter = 0
-		constructor(nombre) {
-			// ¿POR QUÉ? contador: Para dar especificidad al momento de individualizar cada tarea (y para así evitar que se borren duplicados).
-			this.contador = ++Tarea.counter */
-	constructor(nombre) {
-		this.nombre = nombre
-		this.tiempoDeCreacion = new Tiempo().localDateTime
-		this.id = `${this.tiempoDeCreacion} - ${this.nombre}`
+	static contador = 0
+	constructor(descripcion, tiempo) {
+		// ¿POR QUÉ? contador: Para dar especificidad al momento de individualizar cada tarea (y para así evitar que se borren duplicados).
+		this.contador = ++Tarea.contador
+		this.descripcion = descripcion
+		this.tiempoDeCreacion = tiempo ? tiempo : new Tiempo().localDateTime
+		this.id = `${this.tiempoDeCreacion} - ${this.contador} - ${this.descripcion}`
 		this.completacion = false
 	}
 }
 
+/* -----------FUNCIONES:----------- */
+
 function instanciarObjetoTarea(entrada) {
+	// Descomentar si deseamos impedir que se agreguen tareas vacías:
 	// if (entrada.value) {
 	if (entrada) {
 		const objetoTarea = new Tarea(entrada.value)
@@ -58,7 +36,7 @@ const crearCheckbox = objetoTarea => {
 	const checkbox = document.createElement("input")
 	checkbox.setAttribute("type", "checkbox")
 	// DESACTIVADO (para testearlo):
-	// checkbox.setAttribute("name", objetoTarea.nombre)
+	// checkbox.setAttribute("name", objetoTarea.descripcion)
 	// ¿POR QUÉ? id: Para que la etiqueta pueda identificar y reconocer a su checkbox:
 	checkbox.id = objetoTarea.id
 	checkbox.addEventListener("change", () => objetoTarea.completacion = checkbox.checked)
@@ -68,10 +46,11 @@ const crearEtiqueta = (checkbox, objetoTarea) => {
 	const etiqueta = document.createElement("label")
 	// ¿POR QUÉ? for: Para que la etiqueta se aplique a su checkbox respectivo:
 	etiqueta.setAttribute("for", checkbox.id)
-	etiqueta.innerText = objetoTarea.nombre
+	etiqueta.innerText = objetoTarea.descripcion
 	return etiqueta
 }
-const crearDeleteBtn = () => {
+const crearDeleteBtn = objetoTarea => {
+	// const crearDeleteBtn = () => {
 	const deleteBtn = document.createElement("button")
 	// DESACTIVADO (para testearlo):
 	// ¿POR QUÉ? type: Para evitar que se active su comportamiento por defecto ("submit" dentro de un formulario). 
@@ -79,14 +58,21 @@ const crearDeleteBtn = () => {
 	// DESACTIVADO (para testearlo):
 	// deleteBtn.className = "deleteBtn"
 	deleteBtn.innerText = "X"
-	deleteBtn.addEventListener("click", () => deleteBtn.parentNode.remove())
+	deleteBtn.addEventListener("click", () => {
+		deleteBtn.parentNode.remove()
+		// Eliminar objetoTarea de arrayDeTareas:
+		arrayDeTareas = arrayDeTareas.filter(el => el.id !== objetoTarea.id)
+		// Eliminar objetoTarea de Storage:
+		localStorage.removeItem(objetoTarea.id)
+	})
 	return deleteBtn
 }
 
 function crearTareaHTML(objetoTarea) {
 	const checkbox = crearCheckbox(objetoTarea)
 	const etiqueta = crearEtiqueta(checkbox, objetoTarea)
-	const deleteBtn = crearDeleteBtn()
+	const deleteBtn = crearDeleteBtn(objetoTarea)
+	// const deleteBtn = crearDeleteBtn()
 
 	const tareaHTML = document.createElement("li")
 	// DESACTIVADO (para testearlo):
@@ -95,36 +81,96 @@ function crearTareaHTML(objetoTarea) {
 	return tareaHTML
 }
 
-function anexarTareaHTML(evento, entrada, listaHTML) {
-	/* ¿POR QUÉ?
+function agregarNuevaTarea(evento, entrada, arrayDeTareas, listaHTML) {
+	// ¿POR QUÉ?
 	// evento.key: Necesito comprobar que la tecla pulsada en el evento "keydown" dentro del "input[type='text']" sea la tecla Enter.
 	// evento.type: Si dejo sólo la condición "evento.key", no podrá ser escuchado el evento "click" del "button" que agrega tareas, porque no pasará la condición mencionada. Por eso, me veo obligado a poner esta segunda condición (alternativa).
-	*/
+
 	if (evento.key === "Enter" || evento.type === "click") {
 		const objetoTarea = instanciarObjetoTarea(entrada)
+		arrayDeTareas.push(objetoTarea)
+
 		const tareaHTML = crearTareaHTML(objetoTarea)
 		listaHTML.appendChild(tareaHTML)
+
+		localStorage.setItem(objetoTarea.id, JSON.stringify(objetoTarea))
 	}
 }
 
+function recuperarObjetosTareaDesdeStorage() {
+	for (let i = 0; i < sessionStorage.length; i++) {
+		const objetoTareaStorage = JSON.parse(sessionStorage.getItem(sessionStorage.key(i)))
+		const objetoTarea = new Tarea(objetoTareaStorage.descripcion,objetoTareaStorage.tiempoDeCreacion)
+		lista.push(objetoTarea)
+	}
+	// La función pasada como argumento a "*.sort(*)" debe ser acorde con el tipo de valor que intentamos ordenar (en este caso - objetoTarea.id - , NO una resta, sino que una COMPARACIÓN):
+	lista.sort((a, b) => a.id < b.id ? -1 : 1)
+	return lista
+}
+/* const anexarTareasDeStorageAHTML = (listaStorage, listaHTML) => {
+	listaStorage.forEach(tarea => {
+		const tareaHTML = crearTareaHTML(tarea, listaStorage)
+		if (tareaHTML) listaHTML.appendChild(tareaHTML)
+	})
+} */
+
 /* ----------INVOCACIONES:---------- */
 
-const listaHTML = document.querySelector("ul")
-const entrada = document.querySelector("input[type='text']")
-entrada.addEventListener("keydown", evento => anexarTareaHTML(evento, entrada, listaHTML))
-const boton = document.querySelector("button#addTaskBtn")
-boton.addEventListener("click", evento => anexarTareaHTML(evento, entrada, listaHTML))
+let arrayDeTareas = []
+// Versión "static":
+// const listaHTML = document.querySelector("ul")
+// Versión "live":
+const listaHTML = document.getElementsByTagName("ul")[0]
 
-/* --------------------------------- */
+const entrada = document.querySelector("input[type='text']")
+entrada.addEventListener("keydown", evento => agregarNuevaTarea(evento, entrada, arrayDeTareas, listaHTML))
+
+const boton = document.querySelector("button#addTaskBtn")
+boton.addEventListener("click", evento => agregarNuevaTarea(evento, entrada, arrayDeTareas, listaHTML))
+
+/* -----------MISCELÁNEO:---------- */
 
 /* // EXPERIMENTOS DE RESUCITACIÓN ("JSON.parse() revive function"):
 const tareaRevivida = JSON.parse(`{
-	"nombre": "b",
-	"completacion": false,
-	"tiempoDeCreacion": "28-12-2023, 17:25:57",
-	"id": "28-12-2023, 17:25:57 - b",
-	"esTarea": true
-}`,	(key, value) => key === "nombre" ? new Tarea(value) : undefined)
+	 "contador": 1, 
+	 "descripcion": "a", 
+	 "tiempoDeCreacion": "29-12-2023, 00:09:51", 
+	 "id": "29-12-2023, 00:09:51 - 1 - a", 
+	 "completacion": false 
+	}`,
+	// (key, value) => key === "descripcion" ? new Tarea(value) : undefined)
+	(key, value) => {
+		if (value === "1") {
+			return value
+		} else {
+			return false
+		}
+		// console.log(key);
+	})
 console.log(tareaRevivida) */
-
-// INSPIRADO EN https://www.w3schools.com/howto/howto_js_todolist.asp
+/* // FUNCIONES LOCAS PARA CONVERTIR MINUTOS/HORAS/DÍAS A MILISEGUNDOS (Y VICEVERSA):
+function timeToMS(time, timeUnit) {
+	const minutesToMS = minutes => minutes * 60 * 1000
+	const hoursToMS = hours => hours * minutesToMS(60)
+	const daysToMS = days => days * hoursToMS(24)
+	const timeUnits = [
+		{ name: "m", toMSFunc: minutesToMS, },
+		{ name: "h", toMSFunc: hoursToMS, },
+		{ name: "d", toMSFunc: daysToMS, }
+	]
+	const unit = timeUnits.find(unit => unit.name === timeUnit)
+	return unit.toMSFunc(time)
+}
+function msToTime(ms, timeUnit) {
+	const msToMinutes = ms => ms / (60 * 1000)
+	const msToHours = ms => msToMinutes(ms) / 60
+	const msToDays = ms => msToHours(ms) / 24
+	const timeUnits = [
+		{ name: "m", toTimeUnitFunc: msToMinutes, },
+		{ name: "h", toTimeUnitFunc: msToHours, },
+		{ name: "d", toTimeUnitFunc: msToDays, }
+	]
+	const unit = timeUnits.find(unit => unit.name === timeUnit)
+	return unit.toTimeUnitFunc(ms)
+}
+*/
