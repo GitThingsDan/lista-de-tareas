323a0523f64f28a1670bf36641cf1bec8c2036e4
GitThingsDan <dvillarroela@proton.me>
2024-01-05 20:13:48 -0300

    feat: contador de tareas es ahora universal, y se persiste en Storage

--- js/index.js
+++ js/index.js
@@ -10,16 +10,16 @@ class Tiempo {
 	}
 }
 class Tarea {
-	/** Aquí hay un PROBLEMA: al recuperar las 'Tarea's cada vez que el sitio web se recarga, el contador parte desde 0 de nuevo, cosa que no debería ser.*/
-	static contador = 0
+	/** "Tarea.contador" (nuestro "contador universal" de aquí en adelante) debe rescatarse desde Storage, con tal de que persista a través de las recargas del sitio web. */
+	static contador = localStorage.getItem("Contador")
 	constructor(descripcion) {
 		this.tiempoDeCreacion = new Tiempo().localDateTime
-		/** La propiedad "contador" confiere especificidad adicional al momento de individualizar cada tarea (y también evita que al borrar una tarea se borren sus duplicados con ella). */
+		/** La propiedad "contador" de cada objetoTarea confiere especificidad adicional al momento de individualizar cada nueva instancia de "Tarea" (y también evita que al eliminar una tarea se borren sus duplicados - si es que los hubiera - junto con ella). */
 		this.contador = ++Tarea.contador
+		/** Si el {@link Tarea.contador | contador universal} no está en Storage, se creará allí apenas se cree un objetoTarea (sobreescribiéndose con su valor de "contador"). */
+		localStorage.setItem("Contador", this.contador)
 		this.descripcion = descripcion
-		/** Se me complica habilitar la edición de las tareas y que se actualicen sus descripciones pero no sus "id"s en Storage (por otro lado, si las seteo en Storage de todos modos, se creará una tarea nueva cada vez que se edite su descripción, cuando la idea es que se sobreescriba... 🤦🏻‍♂️). Por esto, simplifiqué el "id" sacando la propiedad "descripcion"...¡ojalá que funcione! */
 		this.id = `${this.tiempoDeCreacion} - ${this.contador}`
-		// this.id = `${this.tiempoDeCreacion} - ${this.contador} - ${this.descripcion}`
 		this.completacion = false
 	}
 }
@@ -47,29 +47,35 @@ const crearEtiqueta = (objetoTarea, checkbox) => {
 	/** "El atributo "contenteditable" permite que el texto del elemento sea editable..." - Capitán Obvio, 2024 */
 	etiqueta.setAttribute("contenteditable", true)
 	etiqueta.innerText = objetoTarea.descripcion
-	/** Cada vez que se edite la descripción de la tarea, actualizar la propiedad "descripcion" del objetoTarea respectivo (y también en Storage). */
+	/** Cada vez que se edite la etiqueta de la tareaHTML: */
 	etiqueta.addEventListener("input", () => {
+		/** - Actualizar la propiedad "descripcion" del objetoTarea respectivo. */
 		objetoTarea.descripcion = etiqueta.innerText
+		/** - Actualizar tareaJSON en Storage. */
 		localStorage.setItem(objetoTarea.id, JSON.stringify(objetoTarea))
 	})
 	return etiqueta
 }
-/** @param {any[]} arrayDeTareas - Hubiera sido ideal no haber añadido el parámetro "arrayDeTareas" que añadí para mayor dinamismo al aplicar "filter"...por culpa de esto, tuve que también añadir el parámetro a todas las funciones que dependen de esta... 🤦🏻‍♂️ */
+/** @param {any[]} arrayDeTareas - Hubiera sido ideal no haber añadido el parámetro "arrayDeTareas" que agregué para mayor dinamismo al aplicar "filter"...por culpa de eso, tuve que también añadir el parámetro a todas las funciones que dependen de esta... 🤦🏻‍♂️ */
 const crearDeleteBtn = (arrayDeTareas, objetoTarea) => {
 	const deleteBtn = document.createElement("button")
 	/** La "X" es una forma rápida de simbolizar la función de "eliminar tarea" que este botón representa. */
 	deleteBtn.innerText = "X"
 	deleteBtn.addEventListener("click", () => {
-		/** Eliminar tarea del array de tareas en index.js: */
+		/** Eliminar objetoTarea de arrayDeTareas en index.js: 
+		 * (Aquí hay un PROBLEMA: no se están eliminando los "objetoTarea"s de arrayDeTareas cada vez que se elimina una tarea; muy por el contrario, arrayDeTareas sólo se actualiza cuando se recarga el sitio web. Al parecer tiene que ver con la primera invocación que hago: "let arrayDeTareas = recuperarTareasDesdeStorage()".) 
+		 */
 		arrayDeTareas = arrayDeTareas.filter(el => el.id !== objetoTarea.id)
-		/** Eliminar tarea del documento HTML: */
+		/** Eliminar tareaHTML del DOM: */
 		deleteBtn.parentNode.remove()
-		/** Eliminar tarea del Storage: */
+		/** Eliminar tareaJSON de Storage: */
 		localStorage.removeItem(objetoTarea.id)
+		/** Actualizar contador universal: */
+		localStorage.setItem("Contador", --Tarea.contador)
 	})
 	return deleteBtn
 }
-/** @param {any[]} arrayDeTareas - Hubiera sido ideal no haber añadido el parámetro "arrayDeTareas" que añadí para mayor dinamismo al aplicar "filter" en la función {@link crearDeleteBtn | crearDeleteBtn}...por culpa de esto, tuve que también añadir el parámetro a todas las funciones que dependen de esta... 🤦🏻‍♂️ */
+
 function crearTareaHTML(arrayDeTareas, objetoTarea) {
 	const checkbox = crearCheckbox(objetoTarea)
 	const etiqueta = crearEtiqueta(objetoTarea, checkbox)
@@ -87,7 +93,7 @@ function agregarNuevaTarea(evento, entrada, arrayDeTareas, listaHTML) {
 	 * evento.type: Si sólo dejo la condición "evento.key", no podrá funcionará el evento "click" del "button" que agrega tareas, porque no pasará la condición mencionada. Por eso, me veo obligado a poner esta segunda condición (alternativa).
 	*/
 	if (evento.key === "Enter" || evento.type === "click") {
-		/** Objeto instanciado para añadirse al arrayDeTareas (y, eventualmente, al Storage). */
+		/** Objeto instancia de la clase "Tarea", que será añadido a arrayDeTareas (y, eventualmente, a Storage). */
 		const objetoTarea = new Tarea(entrada.value)
 		arrayDeTareas.push(objetoTarea)
 		/** Se asigna un valor de string vacío ("") al campo de entrada, para "reiniciarlo" y permitir que el usuario ingrese una nueva tarea desde 0. */
@@ -97,7 +103,7 @@ function agregarNuevaTarea(evento, entrada, arrayDeTareas, listaHTML) {
 		const tareaHTML = crearTareaHTML(arrayDeTareas, objetoTarea)
 		listaHTML.appendChild(tareaHTML)
 
-		/** Añadir (por primera vez) objetoTarea al Storage: */
+		/** Crear objetoTarea en Storage: */
 		localStorage.setItem(objetoTarea.id, JSON.stringify(objetoTarea))
 	}
 }
@@ -105,15 +111,22 @@ function agregarNuevaTarea(evento, entrada, arrayDeTareas, listaHTML) {
 function recuperarTareasDesdeStorage() {
 	let arrayDeTareas = []
 	for (let i = 0; i < localStorage.length; i++) {
-		const claveJSONTarea = localStorage.key(i)
-		const JSONtarea = localStorage.getItem(claveJSONTarea)
-		const objetoTareaStorage = JSON.parse(JSONtarea)
-		arrayDeTareas.push(objetoTareaStorage)
+		const claveTareaJSON = localStorage.key(i)
+		/** Con este "if", me aseguro de recuperar sólo las tareas y no el {@link Tarea.contador | contador universal} que guardé en Storage. */
+		if (claveTareaJSON !== "Contador") {
+			const tareaJSON = localStorage.getItem(claveTareaJSON)
+			const objetoTarea = JSON.parse(tareaJSON)
+			arrayDeTareas.push(objetoTarea)
+		}
 	}
 	return arrayDeTareas
 }
 function ordenarYAnexarTareasRecuperadasAHTML(arrayDeTareas, listaHTML) {
-	/** La función pasada como argumento a "*.sort(*)" debe ser acorde con el tipo de valor que intentamos ordenar (en este caso - objetoTarea.id - , NO una resta, sino que una COMPARACIÓN - dado que el "id" comienza con un "contador" numérico, el que será sometido a esta comparación -). */
+	/** La función pasada como argumento a ".sort()" debe ser acorde con el tipo de valor que intentamos ordenar (en este caso - objetoTarea.id - , NO una resta, sino que una COMPARACIÓN, dado que el "id" es un string - de números, pero string de todos modos -).
+ 
+	 * Por cierto, para más adelante, aquí hay una forma más avanzada, abreviada, bonita y menos compleja de hacer este ".sort()" (usando operador condicional - ternario -):
+	 * arrayDeTareas.sort((a, b) => a.id < b.id ? -1 : 1) 
+	*/
 	arrayDeTareas.sort((a, b) => {
 		if (a.id < b.id) {
 			return -1
@@ -121,13 +134,7 @@ function ordenarYAnexarTareasRecuperadasAHTML(arrayDeTareas, listaHTML) {
 			return 1
 		}
 	})
-	/** Forma avanzada, abreviada y menos compleja de hacerlo (operador condicional - ternario -):
-	 * arrayDeTareas.sort((a, b) => a.id < b.id ? -1 : 1) 
-	*/
-	arrayDeTareas.forEach(
-		/** @param {any[]} arr - Hubiera sido ideal no haber añadido el parámetro "arrayDeTareas" que añadí para mayor dinamismo al aplicar "filter" en la función {@link crearDeleteBtn | crearDeleteBtn}...por culpa de esto, tuve que también añadir el parámetro a todas las funciones que dependen de esta... 🤦🏻‍♂️ */
-		(el, idx, arr) => listaHTML.appendChild(crearTareaHTML(arr, el))
-	)
+	arrayDeTareas.forEach((el, idx, arr) => listaHTML.appendChild(crearTareaHTML(arr, el)))
 }
 
 /* ----------INVOCACIONES:---------- */
