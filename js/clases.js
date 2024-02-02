07a769875232612b36a529a989b374a5faceead7
GitThingsDan <dvillarroela@proton.me>
2024-02-02 16:55:34 -0300

    clases.js: agregar comentarios JSDoc para mayor claridad al leer el código, añadir @ts-check y 'use strict' al inicio, y modificar nombre de Tarea.contador por Tarea.contadorGlobal, para así distinguirlo del this.contador de cada instancia de la clase Tarea

--- js/clases.js
+++ js/clases.js
@@ -1,23 +1,60 @@
+// @ts-check
+"use strict"
+
 /* ------------CLASES:------------ */
 
+/** 
+ * Instancias de la clase {@link Tiempo}. 
+ * */
 class Tiempo {
 	constructor() {
+		/** Milisegundos transcurridos desde el 1 de enero de 1970. @type {number} */
 		this.ms = Date.now()
+		/** Objeto "Date", obtenido a partir de {@link ms|milisegundos}. @type {Date} */
 		this.dateTime = new Date(this.ms)
+		/** Fecha y hora actuales, en formato local, obtenido a partir de un {@link dateTime|objeto "Date"}. @type {string} */
 		this.localDateTime = this.dateTime.toLocaleString()
 	}
 }
+
+/** 
+ * Instancias de la clase {@link Tarea}. 
+ * */
 class Tarea {
-	/** "Tarea.contador" (nuestro "contador universal" de aquí en adelante) debe rescatarse desde Storage, con tal de que persista a través de las recargas del sitio web. */
-	static contador = localStorage.getItem("Contador")
+	/** 
+	 * Un acumulador que suma 1 por cada tarea creada.
+	 * 
+	 * Debe rescatarse desde Storage, para que persista a través de las recargas de la página web.
+	 * @type {number}
+	 * */
+	static contadorGlobal = /** @type {?} */ (localStorage.getItem("Contador global"))
+
+	/** 
+	 * @param {string} descripcion Descripción de la tarea.
+	 * */
 	constructor(descripcion) {
+		/** Fecha y hora de creación de la tarea (en formato local). @type {string} */
 		this.tiempoDeCreacion = new Tiempo().localDateTime
-		/** La propiedad "contador" de cada objetoTarea confiere especificidad adicional al momento de individualizar cada nueva instancia de "Tarea" (y también evita que al eliminar una tarea se borren sus duplicados - si es que los hubiera - junto con ella). */
-		this.contador = ++Tarea.contador
-		/** Si el {@link Tarea.contador | contador universal} no está en Storage, se creará allí apenas se cree un objetoTarea (sobreescribiéndose con su valor de "contador"). */
-		localStorage.setItem("Contador", this.contador)
-		this.descripcion = descripcion
+
+		/** 
+		 * Suma 1 a {@link contadorGlobal} y asigna este resultado a sí mismo - {@link Tarea#contador|(this.contador)} -, para representar en qué posición ordinal se creó esta tarea (en un orden ascendente de acuerdo con su {@link tiempoDeCreacion}). 
+		 * 
+		 * Confiere especificidad adicional al momento de individualizar cada instancia de Tarea a partir de su {@link id}, y también evita que al eliminar una tarea se borren sus duplicados (si es que los hubiera) junto con ella. 
+		 * @type {number}
+		 */
+		this.contador = ++Tarea.contadorGlobal
+		/** 
+		 * Si el {@link contadorGlobal} no está en Storage, entonces apenas se instancie una tarea, este contador se creará allí, con el valor de contador de la instancia (es decir, {@link Tarea#contador|this.contador}). 
+		 * */
+		localStorage.setItem("Contador global", /** @type {?} */(this.contador))
+
+		/** Identificador único de la tarea. Es una concatenación de {@link tiempoDeCreacion} y {@link Tarea#contador|(this.contador)}. @type {string} */
 		this.id = `${this.tiempoDeCreacion} - ${this.contador}`
+
+		/** Descripción de la tarea. @type {string} */
+		this.descripcion = descripcion
+
+		/** Indica si la tarea está completada o no. @type {boolean} */
 		this.completacion = false
 	}
 }
